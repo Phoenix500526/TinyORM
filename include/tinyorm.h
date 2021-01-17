223b69289e355be1cd15068ac3ed9b17e8ff39f9
Phoenix <Phoenix500526@163.com>
2021-01-17 16:37:13 +0800

    添加可空类型的实现，并提供一套序类化与反序列化机制，用于将C++语言类型转换为字符串

--- include/tinyorm.h
+++ include/tinyorm.h
@@ -1,32 +1,119 @@
 #ifndef TINYORM_H_
 #define TINYORM_H_
 
+#include <cstddef>
+#include <map>
+#include <sstream>
 #include <string>
 #include <string_view>
 #include <type_traits>
 #include <vector>
 
-#define REFLECTION(_TABLE_NAME_, ...)                                     \
- private:                                                             \
-  friend class tinyorm_impl::ReflectionVisitor;                         \
-  template <typename Fn>                                              \
-  inline auto __Apply(const Fn& fn) {                                 \
-    return fn(__VA_ARGS__);                                           \
-  }                                                                   \
-  template <typename Fn>											  \
-  inline auto __Apply(const Fn& fn) const { return fn(__VA_ARGS__); } \
-  constexpr static const char* __TableName = _TABLE_NAME_;            \
-  constexpr static const char* __FieldNames = #__VA_ARGS__;	//!< Generate reflection information for user-defined class
+#define REFLECTION(_TABLE_NAME_, ...)                      \
+ private:                                                  \
+  friend class tinyorm_impl::ReflectionVisitor;            \
+  template <typename Fn>                                   \
+  inline auto __Apply(const Fn& fn) {                      \
+    return fn(__VA_ARGS__);                                \
+  }                                                        \
+  template <typename Fn>                                   \
+  inline auto __Apply(const Fn& fn) const {                \
+    return fn(__VA_ARGS__);                                \
+  }                                                        \
+  constexpr static const char* __TableName = _TABLE_NAME_; \
+  constexpr static const char* __FieldNames =              \
+      #__VA_ARGS__;  //!< Generate reflection information for
+                     //!< user-defined class
 
 #define NO_REFLECTIONED \
   "Please Inject the metainformation of your class by `REFLECTION` first"
-#define NO_SUCH_FIELD \
-  "No such field"
+#define NO_SUCH_FIELD "No such field"
+#define BAD_TYPE "Invalid Type"
+#define NULL_DESERIALIZE "Cannot deserialize NULL value to a non-nullable value"
+
+namespace tinyorm {
+
+/**
+ * @brief Nullable is wrapper class.
+ * @details
+ *  - A Nullable object is a equivalent to the data whose value can be NULL in
+ * database.
+ *  - All the Nullable objects are value semantics, they are copyable,
+ * moveable.
+ */
+template <typename T>
+class Nullable {
+ private:
+  bool hasValue_;
+  T value_;
+
+  template <typename T2>
+  friend bool operator==(const Nullable<T2>& op1, const Nullable<T2>& op2);
+  template <typename T2>
+  friend bool operator==(const Nullable<T2>& op1, const T2& op2);
+  template <typename T2>
+  friend bool operator==(const T2& op1, const Nullable<T2>& op2);
+  template <typename T2>
+  friend bool operator==(const Nullable<T2>& op1, std::nullptr_t);
+  template <typename T2>
+  friend bool operator==(std::nullptr_t, const Nullable<T2>& op2);
+
+ public:
+  Nullable() : hasValue_(false), value_(T()) {}
+  Nullable(std::nullptr_t) : Nullable() {}
+  Nullable(const T& value) : hasValue_(true), value_(value) {}
+  ~Nullable() = default;
+
+  Nullable<T> operator=(std::nullptr_t) {
+    hasValue_ = false;
+    value_ = T();
+    return *this;
+  }
+
+  Nullable<T> operator=(const T& value) {
+    hasValue_ = true;
+    value_ = value;
+    return *this;
+  }
+
+  inline bool HasValue() const { return hasValue_; }
+
+  inline const T& Value() const { return value_; }
+};
+
+template <typename T2>
+bool operator==(const Nullable<T2>& op1, const Nullable<T2>& op2) {
+  return (!op1.HasValue() && !op2.HasValue()) ||
+         ((op1.HasValue() == op2.HasValue()) && (op1.Value() == op2.Value()));
+}
+
+template <typename T2>
+bool operator==(const Nullable<T2>& op1, const T2& op2) {
+  return op1.HasValue() && op1.Value() == op2;
+}
+
+template <typename T2>
+bool operator==(const T2& op1, const Nullable<T2>& op2) {
+  return operator==(op2, op1);
+}
+
+template <typename T2>
+bool operator==(const Nullable<T2>& op1, std::nullptr_t) {
+  return !op1.HasValue();
+}
+
+template <typename T2>
+bool operator==(std::nullptr_t, const Nullable<T2>& op2) {
+  return operator==(op2, nullptr);
+}
+
+}  // namespace tinyorm
 
 namespace tinyorm_impl {
 /**
  * @brief A visitor for reflection
- * @details User shouldn't access the reflection information directly, cuz that will spoil the encapsulation.
+ * @details User shouldn't access the reflection information directly, cuz that
+ * will spoil the encapsulation.
  */
 class ReflectionVisitor {
  private:
@@ -69,70 +156,169 @@ class ReflectionVisitor {
   }
 
   template <typename C>
-  inline static const std::vector<std::string>& FieldNames(const C&){
-  	static const auto fieldNames = ExtractFieldName(C::__FieldNames);
-  	return fieldNames;
+  inline static const std::vector<std::string>& FieldNames(const C&) {
+    static const auto fieldNames = ExtractFieldName(C::__FieldNames);
+    return fieldNames;
+  }
+  /**
+   * @brief Vistor can iterate over the field names which defined in a class
+   * @details [long description]
+   *
+   * @param obj
+   * @param f f is a callable object which receives a variadic parameter list.
+   */
+  template <typename C, typename Fn>
+  inline static auto Visit(C& obj, const Fn& f) {
+    return obj.__Apply(f);
+  }
+};
+
+/**
+ * @brief Mapping C++ data types to SQL data types
+ * @details TypeString will checking type string during compile time. It
+ * supports three C++ data types:
+ * - All integral but any char types.
+ * - All floating type
+ * - string type
+ */
+template <typename T>
+struct TypeString {
+  static constexpr const char* type_string =
+      std::is_integral_v<T> && !std::is_same<T, char>::value &&
+              !std::is_same<T, char16_t>::value &&
+              !std::is_same<T, char32_t>::value &&
+              !std::is_same<T, wchar_t>::value &&
+              !std::is_same<T, unsigned char>::value
+          ? " integer"
+          : std::is_floating_point_v<T>
+                ? " real"
+                : std::is_same<T, std::string>::value ? " text" : nullptr;
+  static_assert(type_string != nullptr, BAD_TYPE);
+};
+
+template <typename T>
+struct TypeString<tinyorm::Nullable<T>> : TypeString<T> {};
+
+class Serializer {
+ public:
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string == nullptr, bool>
+  Serialize(std::ostream& os, const T& value) {}
+
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string != nullptr, bool>
+  Serialize(std::ostream& os, const T& value) {
+    os << value;
+    return true;
   }
 
-  template <typename C, typename Fn>
-  inline static auto Visit(C& obj, const Fn& f){
-  	return obj.__Apply(f);
+  inline static bool Serialize(std::ostream& os, const std::string& value) {
+    os << "'" << value << "'";
+    return true;
+  }
+
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string != nullptr, bool>
+  Serialize(std::ostream& os, const tinyorm::Nullable<T>& value) {
+    if (value.HasValue()) {
+      return Serialize(os, value.Value());
+    }
+    return false;
+  }
+};
+
+class Deserializer {
+ public:
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string == nullptr, void>
+  Deserialize(T&, const char*) {}
+
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string != nullptr, void>
+  Deserialize(T& property, const char* value) {
+    if (value) {
+      std::istringstream{value} >> property;
+    } else {
+      throw std::runtime_error{NULL_DESERIALIZE};
+    }
+  }
+
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string != nullptr, void>
+  Deserialize(std::string& property, const char* value) {
+    if (value) {
+      property = value;
+    } else {
+      throw std::runtime_error{NULL_DESERIALIZE};
+    }
+  }
+
+  template <typename T>
+  inline static std::enable_if_t<TypeString<T>::type_string != nullptr, void>
+  Deserialize(tinyorm::Nullable<T>& property, const char* value) {
+    if (value) {
+      T res;
+      Deserialize(res, value);
+      property = res;
+    } else {
+      property = nullptr;
+    }
   }
 };
 
 }  // namespace tinyorm_impl
 
-
-namespace tinyorm{
+namespace tinyorm {
 /**
  * @brief Extract fields from the given object for the JOIN operation.
  */
-class FieldExtractor
-{
-private:
-	using pair_type = std::pair<const std::string&, const std::string&>;
-	template <typename C>
-	using HasInjected = tinyorm_impl::ReflectionVisitor::HasInjected<C>;
-	std::unordered_map<const void *, pair_type> fieldCache_;
+class FieldExtractor {
+ private:
+  using pair_type = std::pair<const std::string&, const std::string&>;
+  template <typename C>
+  using HasInjected = tinyorm_impl::ReflectionVisitor::HasInjected<C>;
+  std::unordered_map<const void*, pair_type> fieldCache_;
 
-	template <typename T>
-	const pair_type& Get(const T& field) const{
-		try{
-			return fieldCache_.at((const void*)&field);
-		}catch(...){
-			throw(std::runtime_error(NO_SUCH_FIELD));
-		}
-	}
+  template <typename T>
+  const pair_type& Get(const T& field) const {
+    try {
+      return fieldCache_.at((const void*)&field);
+    } catch (...) {
+      throw(std::runtime_error(NO_SUCH_FIELD));
+    }
+  }
 
-	template <typename C>
-	std::enable_if_t<!HasInjected<C>::value> Extract(const C&){}
+  template <typename C>
+  std::enable_if_t<!HasInjected<C>::value> Extract(const C&) {}
 
-	template <typename C>
-	std::enable_if_t<HasInjected<C>::value> Extract(const C& obj){
-		tinyorm_impl::ReflectionVisitor::Visit(obj, 
-			[this, &obj](const auto&... args){
-				const auto& tableName = tinyorm_impl::ReflectionVisitor::TableName(obj);
-				const auto& fieldNames = tinyorm_impl::ReflectionVisitor::FieldNames(obj);
-				int index = 0;
-				(fieldCache_.emplace(&args, pair_type{fieldNames[index++], tableName}),...);
-			}
-		);
-	}
+  template <typename C>
+  std::enable_if_t<HasInjected<C>::value> Extract(const C& obj) {
+    tinyorm_impl::ReflectionVisitor::Visit(obj, [this,
+                                                 &obj](const auto&... args) {
+      const auto& tableName = tinyorm_impl::ReflectionVisitor::TableName(obj);
+      const auto& fieldNames = tinyorm_impl::ReflectionVisitor::FieldNames(obj);
+      int index = 0;
+      (fieldCache_.emplace(&args, pair_type{fieldNames[index++], tableName}),
+       ...);
+    });
+  }
 
-public:
-	template <typename... Classes>
-	FieldExtractor(const Classes&... args){
-		((Extract(args)), ...);
-	}
-	~FieldExtractor() = default;
-	
-	template <typename T>
-	inline const pair_type& operator()(const T& field) const{
-		return Get(field);
-	}
+ public:
+  template <typename... Classes>
+  FieldExtractor(const Classes&... args) {
+    ((Extract(args)), ...);
+  }
+  ~FieldExtractor() = default;
+
+  template <typename T>
+  inline const pair_type& operator()(const T& field) const {
+    return Get(field);
+  }
 };
+
 }  // namespace tinyorm
 
 #undef NO_SUCH_FIELD
 #undef NO_REFLECTIONED
+#undef BAD_TYPE
 #endif  // TINYORM_H_
---
+++ test/Types_Unittest.cc
@@ -0,0 +1,132 @@
+#include <gtest/gtest.h>
+
+#include "tinyorm.h"
+
+using namespace std;
+using namespace tinyorm;
+using namespace tinyorm_impl;
+
+class TypesUnittest : public ::testing::Test {
+ public:
+  TypesUnittest()
+      : var_1(), var_2(nullptr), var_3(3.14159), var_4("HelloWorld") {}
+  ~TypesUnittest() = default;
+
+ protected:
+  Nullable<int> var_1;
+  Nullable<double> var_2;
+  Nullable<double> var_3;
+  Nullable<string> var_4;
+};
+
+struct SerializeTest {
+  SerializeTest()
+      : a(0), b(10), c(100), d(2.71), e(3.14), str("Hello"), nothing() {}
+  int a;
+  long b;
+  long long c;
+  float d;
+  double e;
+  Nullable<string> str;
+  Nullable<string> nothing;
+  REFLECTION("SerializeTest", a, b, c, d, e, str, nothing);
+};
+
+TEST_F(TypesUnittest, NullableConstructTest) {
+  EXPECT_EQ(var_1.HasValue(), false);
+  EXPECT_EQ(var_2.HasValue(), false);
+  EXPECT_EQ(var_3.HasValue(), true);
+  EXPECT_EQ(var_3.Value(), 3.14159);
+  auto tmp_1 = var_3;
+  EXPECT_EQ(tmp_1.HasValue(), true);
+  EXPECT_EQ(tmp_1.Value(), 3.14159);
+  auto tmp_2 = std::move(tmp_1);
+  EXPECT_EQ(tmp_2.HasValue(), true);
+  EXPECT_EQ(tmp_2.Value(), 3.14159);
+  var_2 = 2.71;
+  EXPECT_EQ(var_2.HasValue(), true);
+  EXPECT_EQ(var_2.Value(), 2.71);
+  var_3 = 2;
+  EXPECT_EQ(var_3.HasValue(), true);
+  EXPECT_EQ(var_3.Value(), 2.0);
+  var_1 = 3.14;
+  EXPECT_EQ(var_1.HasValue(), true);
+  EXPECT_EQ(var_1.Value(), 3);
+  EXPECT_EQ(var_4.HasValue(), true);
+  EXPECT_EQ(var_4.Value(), string("HelloWorld"));
+}
+
+TEST_F(TypesUnittest, NullableCompareTest) {
+  Nullable<int> tmp_1(nullptr);
+  EXPECT_TRUE(var_1 == tmp_1);
+  tmp_1 = 10;
+  EXPECT_FALSE(var_1 == tmp_1);
+  var_1 = tmp_1;
+  EXPECT_TRUE(var_1 == tmp_1);
+  auto str = string("HelloWorld");
+  EXPECT_TRUE(var_4 == str && str == var_4);
+  EXPECT_TRUE(var_2 == nullptr && nullptr == var_2);
+}
+
+TEST_F(TypesUnittest, TypeStrTest) {
+  bool a;
+  char b;
+  int c;
+  long d;
+  long long e;
+  EXPECT_STREQ(TypeString<decltype(a)>::type_string, " integer");
+  // EXPECT_STREQ(TypeString<decltype(b)>::type_string," integer"); //compile
+  // error
+  EXPECT_STREQ(TypeString<decltype(c)>::type_string, " integer");
+  EXPECT_STREQ(TypeString<decltype(d)>::type_string, " integer");
+  EXPECT_STREQ(TypeString<decltype(e)>::type_string, " integer");
+
+  float h;
+  double i;
+  long double j;
+  EXPECT_STREQ(TypeString<decltype(h)>::type_string, " real");
+  EXPECT_STREQ(TypeString<decltype(i)>::type_string, " real");
+  EXPECT_STREQ(TypeString<decltype(j)>::type_string, " real");
+
+  string str;
+  EXPECT_STREQ(TypeString<decltype(str)>::type_string, " text");
+
+  EXPECT_STREQ(TypeString<decltype(var_1)>::type_string, " integer");
+  EXPECT_STREQ(TypeString<decltype(var_2)>::type_string, " real");
+  EXPECT_STREQ(TypeString<decltype(var_4)>::type_string, " text");
+}
+
+TEST_F(TypesUnittest, TypeSerializerTest) {
+  SerializeTest st;
+  std::ostringstream os;
+  ReflectionVisitor::Visit(st, [&os](const auto&... value) {
+    (Serializer::Serialize(os << ' ', value), ...);
+  });
+  EXPECT_EQ(os.str(), string(" 0 10 100 2.71 3.14 'Hello' "));
+  int a = 1;
+  float f = 4.77;
+  string str("GTEST");
+  os.str("");
+  Serializer::Serialize(os, a);
+  string res_a = os.str();
+  os.str("");
+  EXPECT_EQ(res_a, string("1"));
+
+  Serializer::Serialize(os, f);
+  string res_f = os.str();
+  os.str("");
+  EXPECT_EQ(res_f, string("4.77"));
+
+  Serializer::Serialize(os, str);
+  string res_str = os.str();
+  os.str("");
+  EXPECT_EQ(res_str, string("'GTEST'"));
+
+  int des_a;
+  float des_f;
+  string des_str;
+  Deserializer::Deserialize(des_a, res_a.c_str());
+  EXPECT_EQ(des_a, a);
+  Deserializer::Deserialize(des_f, res_f.c_str());
+  EXPECT_EQ(des_f, f);
+}
